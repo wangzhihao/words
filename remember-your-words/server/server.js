7dfdc50af3f2a99949e504842eeebe0bd60368b6
wang zhihao <accept.acm@gmail.com>
2015-01-03 17:44:56 +0800

    add a static page

--- remember-your-words/server/server.js
+++ remember-your-words/server/server.js
@@ -7,6 +7,13 @@ var app = module.exports = loopback();
 // Sub-apps like REST API are mounted via boot scripts.
 boot(app, __dirname);
 
+// -- Mount static files here--
+// All static middleware should be registered at the end, as all requests
+// passing the static middleware are hitting the file system
+// Example:
+var path = require('path');
+app.use(loopback.static(path.resolve(__dirname, '../client')));
+
 app.start = function() {
   // start the web server
   return app.listen(function() {
