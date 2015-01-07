aad4fcd6c315e322815cf476e6936a87849b4e78
wang zhihao <accept.acm@gmail.com>
2015-01-07 20:38:31 +0800

    add a run shell script

--- run.sh
+++ run.sh
@@ -2,20 +2,20 @@
 
 echo start the mongo db
 
-WORD_DIR=/Users/wangzhihao/Workspace/words/site
+ROOT_DIR=/Users/wangzhihao/Workspace/words
 
-SERVER_DIR=$WORD_DIR/remember-your-words/server
+SERVER_DIR=$ROOT_DIR/site/remember-your-words/server
 
 echo start the database
-cd $WORD_DIR
+cd $ROOT_DIR
 # run it in background(&) and redirect the output to logs(>>)
-mongod --dbpath ./database/ >> $WORD_DIR/logs/db.log &
+mongod --dbpath $ROOT_DIR/database/ >> $ROOT_DIR/logs/db.log &
 
 
 echo start the server
 cd $SERVER_DIR
 # run it in background(&) and redirect the output to logs(>>)
-slc run >> $WORD_DIR/logs/server.log &
+slc run >> $ROOT_DIR/logs/server.log &
 
 #wait the database and server background processes compelte
 wait
