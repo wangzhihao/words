53b95ad5335042b06ce3a28b1e56b7926d1f094e
wang zhihao <accept.acm@gmail.com>
2015-01-14 13:42:29 +0800

    add some comments

--- remember-your-words/common/models/word.js
+++ remember-your-words/common/models/word.js
@@ -1,3 +1,10 @@
 module.exports = function(Word) {
 
+  /*
+   * Extend the API by creating a new remote method.
+   * More information see http://docs.strongloop.com/display/public/LB/Remote+methods
+   * 
+   * The following doc can help to manipulate data in NodeJs.
+   * http://docs.strongloop.com/display/public/LB/Querying+data
+   */
 };
