495b42ad1458fbf0a27e0973ee512b3ced1ca95d
wang zhihao <accept.acm@gmail.com>
2015-03-23 12:55:08 +0800

    refactor out the db start scripts to the upper level folder

--- run.sh
+++ run.sh
@@ -4,13 +4,6 @@ ROOT_DIR=$( cd "$( dirname "$0" )" && pwd )
 
 SERVER_DIR=$ROOT_DIR/server
 
-echo start the database
-cd $ROOT_DIR
-# run it in background(&) and redirect the output to logs(>>)
-mongod --dbpath $ROOT_DIR/database/ &> $ROOT_DIR/logs/db.log &
-# save the pid of database.
-echo $! > $ROOT_DIR/logs/db.pid
-
 echo start the server
 cd $SERVER_DIR
 # run it in background(&) and redirect the output to logs(>>)
--- shutdown.sh
+++ shutdown.sh
@@ -2,11 +2,6 @@
 
 ROOT_DIR=$( cd "$( dirname "$0" )" && pwd )
 
-echo shut down the mongo db
-
-kill $(cat $ROOT_DIR/logs/db.pid)
-
-
 echo shut down the web server
 
 kill $(cat $ROOT_DIR/logs/server.pid)
