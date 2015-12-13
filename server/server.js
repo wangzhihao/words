aca96ea360e9635ff8b264776dec9d498fecd820
zhihao wang <hao@opentown.cn>
2015-12-14 01:07:03 +0800

    change port

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(loopback.static(path.resolve(__dirname, '../client')));
 
 app.start = function() {
   // start the web server
-  return app.listen(80, function() {
+  return app.listen(3010, function() {
     app.emit('started');
     console.log('Web server listening at: %s', app.get('url'));
   });
